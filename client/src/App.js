584f1cc6b39430862c880aa7b4b96f15118c94d7
Pritam Maharjan <primgdev@gmail.com>
2025-07-29 19:06:51 +1000

    frontend and backend setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,32 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const [menu, setMenu] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/api/menu')
+      .then((res) => setMenu(res.data))
+      .catch((err) => console.error(err));
+  }, []);
+
+
   return (
-   <h1 className="text-lg font-bold underline">
-      Hello world!
-    </h1>
+    <div className='bg-black'>
+      <h2 className='font-bold text-white text-xl'>Menu</h2>
+      <ul className='text-white '>
+        {menu.map(item =>
+        (
+          <li key={item.id}>
+            {item.name} - ${item.price}
+          </li>
+        )
+        )}
+      </ul>
+    </div>
+
   );
 }
 
