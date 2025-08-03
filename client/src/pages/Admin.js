67ce8be2fd86ad0c03ff421a4a0c989e597b9545
Pritam Maharjan <primgdev@gmail.com>
2025-08-03 17:37:21 +1000

    deleting order with id

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -11,15 +11,32 @@ const Admin = () => {
             .catch(err => console.error(err));
     }, []);
 
+
+    const handleDelete = async (orderId) => {
+        // const confirmDelete = window.confirm("Are you sure you want to delete this order?");
+        // if (!confirmDelete) return;
+
+        try {
+            const response = await axios.delete(`http://localhost:3001/api/order/deleteOrder/${orderId}`);
+            alert(response.data.message);
+
+            // Refresh the order list after deletion
+            setOrder(prev => prev.filter(order => order._id !== orderId));
+        } catch (err) {
+            console.error("Failed to delete order", err);
+            alert("Failed to delete order");
+        }
+    };
     return (
         <div>
             <div className='bg-white'>
 
                 {order.map(order => (
-                    <div key={order._id} className='border-b py-2'>
+                    <div key={order._id} className='border-b py-2 m-2'>
                         <p><strong>Name:</strong> {order.name}</p>
                         <p><strong>Phone:</strong> {order.phone}</p>
                         <p><strong>Item:</strong> {order.item}</p>
+                        <button onClick={()=>handleDelete(order._id)} className="p-1 rounded-md bg-red-600 text-white shadow-xl m-1 border ">Delete</button>
                     </div>
                 ))}
 
