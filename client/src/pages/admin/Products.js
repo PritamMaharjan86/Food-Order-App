a7f3e894d2688dd7b4ed2e635dd0b6863b8170a4
Pritam Maharjan <primgdev@gmail.com>
2025-10-16 20:26:43 +1100

    adding button on product section

--- client/src/pages/admin/Products.js
+++ client/src/pages/admin/Products.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios';
+import { IoMdAdd } from "react-icons/io";
+
 
 const Products = () => {
     const [menu, setMenu] = useState([]);
@@ -17,8 +19,11 @@ const Products = () => {
             <h1 className="text-black font-semibold text-2xl fixed bg-white w-11/12 h-15 p-4 rounded-lg shadow-md">
                 Products
             </h1>
-
-            <div className='flex flex-row justify-between m-8 p-4 mt-20'>
+            <div className='mt-20 flex justify-end mr-5'>
+                <button className='shadow-xl flex-row flex items-center gap-1 shadow-gray-300 bg-green-500 w-fit text-gray-100 py-1 rounded-lg px-3'> <IoMdAdd />
+                    Add</button>
+            </div>
+            <div className='flex flex-row justify-between p-4'>
 
                 <table className='text-black min-w-full border border-gray-300 rounded-xl overflow-hidden'>
                     <thead className='bg-gray-200 text-left'>
@@ -41,8 +46,8 @@ const Products = () => {
                                 <td className="py-2 px-4 border-b border-gray-300 "> <img src={item.image} className='w-24 rounded-lg h-fit' /> </td>
                                 <td className="py-2 px-4 border-b border-gray-300 ">
                                     <div className='flex flex-row gap-2 '>
-                                        <button className='bg-red-500 w-1/4 text-gray-200 py-1 rounded-lg px-3'>Delete</button>
-                                        <button className='bg-blue-500 w-1/4 text-gray-200 py-1 rounded-lg px-3'>Edit</button>
+                                        <button className=' bg-red-500 w-1/4 text-gray-200 py-1 rounded-lg px-3'>Delete</button>
+                                        <button className=' bg-blue-500 w-1/4 text-gray-200 py-1 rounded-lg px-3'>Edit</button>
                                     </div>
                                 </td>
                             </tr>
